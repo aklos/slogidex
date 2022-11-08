9cf8eb36bb34f2edbe2a86d55571d91ad27e6532
Alex Klos <alexklos@prohobo.dev>
2022-11-08 22:49:45 +0100

    Remove old components

--- src/components/DocumentPanel/DocumentList.tsx
+++
@@ -1,5 +0,0 @@
-import React from "react";
-
-export default function DocumentList() {
-  return <ul></ul>;
-}
--- src/components/DocumentPanel/InstanceList.tsx
+++
@@ -1,5 +0,0 @@
-import React from "react";
-
-export default function InstanceList() {
-  return <ul></ul>;
-}
