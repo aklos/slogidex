97a88a0c6646071501fc71cd6c0c10ef8cc6fe65
Alex Klos <alexklos@prohobo.dev>
2023-02-12 18:11:11 +0100

    MacOS admin command

--- src-tauri/src/runner.rs
+++ src-tauri/src/runner.rs
@@ -14,13 +14,23 @@ fn create_command_builder(
     need_permissions: bool
 ) -> Command {
     let mut command = match need_permissions {
-        true => Command::new("pkexec"),
+        true => match cfg!(linux) {
+            true => Command::new("pkexec"),
+            false => Command::new("osascript"),
+        },
         false => Command::new(&command_string),
     };
     
     if need_permissions == true {
-        command.arg(&command_string);
+        if cfg!(macos) {
+            command.arg("-e");
+            command.arg(format!(r#"do shell script "{}" with administrator privileges"#, &command_string));            
+        } else {
+            command.arg(&command_string);
+        }
     }
+
+    println!("{:?}", command);
     
     // let mut command = Command::new(&command_string);
     // let mut command = Command::new(file);
